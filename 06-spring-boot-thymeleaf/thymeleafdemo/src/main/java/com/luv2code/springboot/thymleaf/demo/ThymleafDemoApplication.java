2639956c14fa279a6060d7f496e028736852bd86
umatt1 <matt.burkhard@gmail.com>
2023-07-25 14:54:05 -0500

    Implements

---
+++ 06-spring-boot-thymeleaf/thymeleafdemo/src/main/java/com/luv2code/springboot/thymleaf/demo/ThymleafDemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.thymleaf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymleafDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymleafDemoApplication.class, args);
+	}
+
+}
---
+++ 06-spring-boot-thymeleaf/thymeleafdemo/src/test/java/com/luv2code/springboot/thymleaf/demo/ThymleafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.thymleaf.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
